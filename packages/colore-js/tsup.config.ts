5cc2b95d91d0f15488c411a57397b198547eda62
Ty Eggen <tycho.eggen@gmail.com>
2024-10-27 19:47:34 -0700

    🧰  build(implementation): minify and bundle for prod builds

--- packages/colore-js/tsup.config.ts
+++ packages/colore-js/tsup.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, type Options } from 'tsup'
 
+const buildEnv = process.env.NODE_ENV ?? 'development'
+
 const getExt: Options['outExtension'] = ({ format }) => {
     let ext = 'js'
 
@@ -11,12 +13,15 @@ const getExt: Options['outExtension'] = ({ format }) => {
 
 const options: Options = {
     entry: ['src/**/*.ts'],
+    outDir: './dist',
     splitting: false,
     sourcemap: false,
     clean: false,
     dts: false,
     outExtension: getExt,
     shims: true,
+    minify: buildEnv === 'production',
+    bundle: buildEnv === 'production'
 }
 
 export default defineConfig(options)
